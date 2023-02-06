23e12220659d7858d2afca5c3998147cd571f657
onkar kotule <onkar.kotule@bitcs.in>
2023-02-06 22:29:38 +0530

    backend mongoose connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useNewUrlParser: true,
   })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);

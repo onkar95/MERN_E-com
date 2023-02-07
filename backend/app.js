96a66be69f9740a550dee04bb816f1ff40565a22
onkar kotule <onkar.kotule@bitcs.in>
2023-02-07 16:32:51 +0530

    push build

--- backend/app.js
+++ backend/app.js
@@ -48,10 +48,10 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Middleware for Errors

a3a6f2cee7f831a82dcb77cd2ab41f8d56ab900d
onkar kotule <onkar.kotule@bitcs.in>
2023-02-07 11:59:33 +0530

    product error

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// const corsOptions = {
-//   // origin: 'https://main--scintillating-brigadeiros-f08220.netlify.app',
-//   origin: 'http://localhost:3000',
-//   credentials: true,            //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// }
+const corsOptions = {
+  origin: 'https://main--scintillating-brigadeiros-f08220.netlify.app',
+  // origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 
 app.use(cors())
 app.use(express.json());

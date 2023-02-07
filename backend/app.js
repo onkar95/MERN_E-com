570391987336a4fcf2ea4b8a312fec972a7462b6
onkar kotule <onkar.kotule@bitcs.in>
2023-02-07 12:31:04 +0530

    cors and proxy

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 const cors = require('cors');
@@ -12,8 +11,7 @@ const errorMiddleware = require("./middleware/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/.env" });
 }
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 const corsOptions = {
   origin: 'https://main--scintillating-brigadeiros-f08220.netlify.app',
@@ -21,8 +19,11 @@ const corsOptions = {
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
+app.use(cors(corsOptions))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors())
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
